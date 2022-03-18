b263c11c2357860b8a901b9e2d35004e67e1150b
TzurV <tzurvaich@gmail.com>
2022-03-18 16:05:17 +0000

    mark xross talk segments - Work in progress

--- AMI/prepare_ami.py
+++ AMI/prepare_ami.py
@@ -1,5 +1,9 @@
 '''
 based on https://github.com/gcunhase/AMICorpusXML
+
+using python-intervals
+https://github.com/AlexandreDecan/portion/blob/1.10.0/README.md
+
 '''
 
 import os
@@ -7,6 +11,58 @@ import argparse
 from pathlib import Path
 from collections import defaultdict
 from xml.dom import minidom
+import intervals as I
+
+
+# ---------------------- LEARN -----------------------------------------------
+'''
+Tzur Remember
+    #xml_info = [minidom.parse(os.path.join(ami_xml_words_dir, file)) for file in xml_files]
+    #print(xml_info)
+    # items =xml_info[0].getElementsByTagName('w')
+    # ------------- XML line ----
+    # <w nite:id="EN2001a.A.words0" starttime="5.57" endtime="5.94">Okay</w>
+    # word -> print(items[0].firstChild.data)
+    #>> <DOM Text node "'Okay'">
+    #>> None
+    # print(items[0].attributes.keys())
+    #>> dict_keys(['nite:id', 'starttime', 'endtime'])
+    #>> None
+    # print(items[0].getAttributeNode('starttime').value)
+    #>> 5.57
+    #>> None
+
+'''
+
+def play_with_intervals():
+    '''
+    based on https://github.com/AlexandreDecan/portion/blob/1.10.0/README.md#python-data-structure-and-operations-for-intervals
+
+    '''
+    print(I.closed(1, 2))
+    
+    I_0_2 = I.closed(0, 2)
+    I_1_3 = I.closed(1, 3)
+    I_4_6 = I.closed(4, 6)
+    
+    print(f"{I_0_2}  {I_1_3} ")
+    
+    print("intervals are automatically simplified:")
+    print(f"{I_0_2}|{I_1_3} = {I_0_2|I_1_3}")
+
+    print("x.intersection(other) or x & other return the intersection of two intervals.")
+    print(f"{I_0_2}&{I_1_3} = {I_0_2&I_1_3}")
+    
+    I_intersection = I_0_2 & I_4_6
+    print(f"{I_0_2}&{I_4_6} = {I_intersection} and is empty={I_intersection.is_empty()}")
+    
+    # x.overlaps(other) tests if there is an overlap between two intervals.
+    
+    pass
+
+#-----------------------------------------------------------------------------
+
+
 
 # get all xml files and group them
 def get_words_xml_files_list(ami_xml_words_dir):
@@ -48,8 +104,34 @@ def creat_speech_segments(XML_data, speaker):
             segments[-1]= {'speaker':speaker, 
                            'starttime':segments[-1]['starttime'], 
                            'endtime':float(elem.getAttributeNode('endtime').value)}
-
+            
     return segments
+
+
+def mark_crosstalk(sorted_segments):
+    segmenst_including_Xtalk = list()
+    
+    prev_intrval = {'speaker': 'None', 
+                       'interval': I.closed(-I.inf, -0.001)}
+    
+    for seg in sorted_segments:
+        cur_intrval = {'speaker':seg['speaker'], 
+                       'interval': I.closed(seg['starttime'], seg['endtime'])}
+        if prev_intrval['interval'].overlaps(cur_intrval['interval']):
+            #print(f"{prev_intrval} <> {cur_intrval}  {prev_intrval['interval'].overlaps(cur_intrval['interval'])}")
+            prev_intrval = {'speaker':'Xtalk', 
+                           'interval': prev_intrval['interval'] | cur_intrval['interval']}
+            #print(f"New Interval {prev_intrval}")
+            
+        else:
+            segmenst_including_Xtalk.append(prev_intrval)
+            prev_intrval = cur_intrval
+
+    # append last one (?)
+    segmenst_including_Xtalk.append(prev_intrval)
+        
+    return segmenst_including_Xtalk
+
 
 def process_group(ami_xml_words_dir, group, xml_files):
     print(f"Group {group}:{xml_files}")
@@ -59,23 +141,14 @@ def process_group(ami_xml_words_dir, group, xml_files):
         speaker = file.split('.')[1]
         XML_data = minidom.parse(os.path.join(ami_xml_words_dir, file))
         speakers_speech_segments += creat_speech_segments(XML_data, speaker)
-        
+
+    # sort 
+    sorted_segments = sorted(speakers_speech_segments, key=lambda k: (k['endtime'], k['starttime'])) 
     print(f"\t>Total number of segments {len(speakers_speech_segments)}")
     
-    #xml_info = [minidom.parse(os.path.join(ami_xml_words_dir, file)) for file in xml_files]
-    #print(xml_info)
-    # items =xml_info[0].getElementsByTagName('w')
-    # ------------- XML line ----
-    # <w nite:id="EN2001a.A.words0" starttime="5.57" endtime="5.94">Okay</w>
-    # word -> print(items[0].firstChild.data)
-    #>> <DOM Text node "'Okay'">
-    #>> None
-    # print(items[0].attributes.keys())
-    #>> dict_keys(['nite:id', 'starttime', 'endtime'])
-    #>> None
-    # print(items[0].getAttributeNode('starttime').value)
-    #>> 5.57
-    #>> None
+    segmenst_including_Xtalk = mark_crosstalk(sorted_segments)
+    print(f"\t>Total number of segments including_Xtalk {len(segmenst_including_Xtalk)}")
+    
 
 
 def process(args):
@@ -88,6 +161,9 @@ def process(args):
         process_group(args.ami_xml_words_dir, group, group_speaker_files[group])
         
     
+
+
+
 
 
 if __name__ == '__main__':
